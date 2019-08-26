6b3b93d7513c6190435fd424ff58329ba25b5cd5
Danil Borchevkin <danil.borchevkin@lab409.ru>
2019-08-27 00:02:30 +0300

    WIP

--- sw_wavelets_pycwt.py
+++ sw_wavelets_pycwt.py
@@ -16,72 +16,83 @@ def get_graph_from_file(in_filepath, out_folder, out_filename):
     # TODO fix this shit
     dat = p1
 
-    # TODO tihnk about kwargs for this values
-    title = "Demo title"
-    label = "Demo label"
-    units = "Demo unit"
+    title = 'NINO3 Sea Surface Temperature'
+    label = 'NINO3 SST'
+    units = 'degC'
 
     # Values for calculations
     # TODO spike about args
-    t0 = 0          # start time
-    dt = 1          # step of differentiation
+    t0 = 12           # start time
+    dt = 0.5          # step of differentiation - in minutes
 
-    # Time array in years - TODO not in yers. think about it
     N = dat.size
     t = numpy.arange(0, N) * dt + t0
 
-    # Differentiation
-    # TODO use enumarete and check odd and evens
-    i = 0
-    dat_diff_list = list()
-    for val in dat:
-        if i == 0:
-            i += 1
-            continue
-        
-        cur_diff = (dat[i] - dat[i-1]) / dt
-        dat_diff_list.append(cur_diff)
-
-        i += 1
-    dat_diff = numpy.asarray(dat_diff_list)
-    t_diff = t[:299]
-
-    # Detrenting - TODO not neccessary
-    # TODO think about neccessary
-    p = numpy.polyfit(t_diff - t0, dat_diff, 1)
-    dat_notrend = dat_diff - numpy.polyval(p, t_diff - t0)
+    p = numpy.polyfit(t - t0, dat, 1)
+    dat_notrend = dat - numpy.polyval(p, t - t0)
     std = dat_notrend.std()  # Standard deviation
     var = std ** 2  # Variance
     dat_norm = dat_notrend / std  # Normalized dataset
-    
-    # Define parameters for wavelet
-    omega_null = 6
-    mother = wavelet.Morlet(omega_null)
-    s0 = 2 * dt     # starting scale
-    dj = 1 / 12     # amount of suboctaves in octave
-    J = 7 / dj      # Seven powers of two with dj sub-octaves
-    alpha, _,_ = wavelet.ar1(dat_norm)  # Lag-1 correlation for red noise
 
-    # Perform wavelet transform
+    mother = wavelet.Morlet(6)
+    s0 = 2 * dt  # Starting scale, in this case 2 * 0.25 years = 6 months
+    dj = 1 / 12  # Twelve sub-octaves per octaves
+    J = 7 / dj  # Seven powers of two with dj sub-octaves
+    alpha, _, _ = wavelet.ar1(dat)  # Lag-1 autocorrelation for red noise
+
     wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(dat_norm, dt, dj, s0, J, mother)
-    iwave = wavelet.icwt(wave, scales, dt, dj, mother)
-    # Inverse wavelet transform # TODO we need normalization
-    #iwave = wavelet.icwt(wave, scales, dt, dj, mother) * std
+    iwave = wavelet.icwt(wave, scales, dt, dj, mother) * std
 
-    # Plot
+    power = (numpy.abs(wave)) ** 2
+    fft_power = numpy.abs(fft) ** 2
+    period = 1 / freqs
+
+    power /= scales[:, None]
+
+    signif, fft_theor = wavelet.significance(1.0, dt, scales, 0, alpha, significance_level=0.95, wavelet=mother)
+    sig95 = numpy.ones([1, N]) * signif[:, None]
+    sig95 = power / sig95
+
+    glbl_power = power.mean(axis=1)
+    dof = N - scales  # Correction for padding at edges
+    glbl_signif, tmp = wavelet.significance(var, dt, scales, 1, alpha, significance_level=0.95, dof=dof, wavelet=mother)
+
+    sel = find((period >= 2) & (period < 8))
+    Cdelta = mother.cdelta
+    scale_avg = (scales * numpy.ones((N, 1))).transpose()
+    scale_avg = power / scale_avg  # As in Torrence and Compo (1998) equation 24
+    scale_avg = var * dj * dt / Cdelta * scale_avg[sel, :].sum(axis=0)
+    scale_avg_signif, tmp = wavelet.significance(var, dt, scales, 2, alpha, significance_level=0.95, dof=[scales[sel[0]], scales[sel[-1]]], wavelet=mother)
+
+    # Prepare the figure
     pyplot.close('all')
-    ax = pyplot.axes()
-    
     #pyplot.ioff()
-    #figprops = dict(figsize=(11, 8), dpi=72)
-    #fig = pyplot.figure(**figprops)
+    figprops = dict(dpi=144)
+    fig = pyplot.figure(**figprops)
 
-    ax.plot(t_diff, iwave, '-', linewidth=1, color=[0.5, 0.5, 0.5])
-    ax.plot(t_diff, dat_diff)
+    # Second sub-plot, the normalized wavelet power spectrum and significance
+    # level contour lines and cone of influece hatched area. Note that period
+    # scale is logarithmic.
+    bx = pyplot.axes([0.1, 0.37, 0.65, 0.28])
+    levels = [0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16]
+    bx.contourf(t, numpy.log2(period), numpy.log2(power), numpy.log2(levels),
+                extend='both', cmap=pyplot.cm.viridis)
+    extent = [t.min(), t.max(), 0, max(period)]
+    bx.contour(t, numpy.log2(period), sig95, [-99, 1], colors='k', linewidths=2,
+            extent=extent)
+    bx.set_title('{} Wavelet Power Spectrum ({})'.format(label, mother.name))
+    bx.set_ylabel('Period (minutes)')
+    #
+    #Yticks = 2 ** numpy.arange(numpy.ceil(numpy.log2(period.min())),
+    #                        numpy.ceil(numpy.log2(period.max())))
+    #bx.set_yticks(numpy.log2(Yticks))
+    #bx.set_yticklabels(Yticks)
+    #bx.set_yticks(Yticks)
+    #bx.set_yticks(period)
 
     # Save graph to file
     # TODO implement
-    #plt.savefig('{}/{}.png'.format(out_folder, out_filename))
+    #pyplot.savefig('{}/{}.png'.format(out_folder, out_filename))
     # ----------------------------------------------
     # or show the graph
     pyplot.show()
